18d0804866b0cb399099addb22adfe0360c5bd27
Nitin Singhal <nitinsinghal.nyu@gmail.com>
2026-05-25 10:44:53 -0400

    Make settings gear button readable (40px, 22px glyph)

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * caches get cleared on activate.
  * ========================================================= */
 
-const CACHE_VERSION = "rs-v9";
+const CACHE_VERSION = "rs-v10";
 
 // Same-origin shell files. Pre-fetched at install time.
 const CORE_ASSETS = [
