8149d75d7146f9a376d632ef67e0ade2753021e9
Nitin Singhal <nitinsinghal.nyu@gmail.com>
2026-06-15 20:05:38 -0400

    Home path: left-align, more top spacing, wider cards

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * caches get cleared on activate.
  * ========================================================= */
 
-const CACHE_VERSION = "rs-v75";
+const CACHE_VERSION = "rs-v76";
 
 // Same-origin shell files. Pre-fetched at install time.
 const CORE_ASSETS = [
